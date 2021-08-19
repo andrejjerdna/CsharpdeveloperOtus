e378eacd624a79c217080c8d7570e3700229e76b
Степанов Андрей <33proekta@mail.ru>
2021-08-19 23:22:05 +0300

    Added DeserializeFromCsvToObject<T>

--- Reflection/Serialize.cs
+++ Reflection/Serialize.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Reflection;
 
@@ -10,25 +12,51 @@ namespace Reflection
         private static readonly string _separator = ",";
         private static readonly string _endRow = "\n";
         private static readonly BindingFlags _bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
-        public static string SerializeFromObjectToCsv(object obj)
+        public static string SerializeFromObjectToCsv(object? obj)
         {
             var objectType = obj.GetType();
-            var properties = objectType.GetProperties(_bindFlags).Select(p => new {name = p.Name, type = p.PropertyType, val = p.GetValue(obj)});
-            var fields = objectType.GetFields(_bindFlags).Select(f => new {name = f.Name, type = f.FieldType, val = f.GetValue(obj)});
+            var properties = objectType.GetProperties(_bindFlags)
+                .Select(p => new {name = p.Name, type = p.PropertyType, val = p.GetValue(obj)});
+            var fields = objectType.GetFields(_bindFlags)
+                .Select(f => new {name = f.Name, type = f.FieldType, val = f.GetValue(obj)});
 
-            var output = properties.Aggregate(string.Empty, (current, property) => current + RowGenerate(property.name, property.type, property.val));
+            var output = properties.Aggregate(string.Empty, (current, property) => 
+                current + RowGenerate(property.name, property.val));
 
-            return fields.Aggregate(output, (current, field) => current + RowGenerate(field.name, field.type, field.val));
+            output = fields.Aggregate(output, (current, field) => 
+                current + RowGenerate(field.name, field.val));
+
+            return output.Trim();
         }
         
-        public static T DeserializeFromCsvToObject<T>(string csv) where T : class
+        public static T DeserializeFromCsvToObject<T>(string csv) where T : new()
         {
-            return null;
+            var targetObject = new T();
+            var targetType = targetObject.GetType();
+
+            var data = csv.Split(_endRow)
+                .Select(r => r.Split(_separator))
+                .Select(v => new {name = v.First(), val = v.Last()});
+
+            foreach (var prop in data)
+            {
+                var propInfo = targetType.GetProperty(prop.name, _bindFlags);
+                propInfo?.SetValue(targetObject,
+                    Convert.ChangeType(prop.val, propInfo.PropertyType), null);
+            }
+            
+            foreach (var prop in data)
+            {
+                var fieldInfo = targetType.GetField(prop.name, _bindFlags);
+                fieldInfo?.SetValue(targetObject, Convert.ChangeType(prop.val, fieldInfo.FieldType));
+            }
+
+            return targetObject;
         }
 
-        private static string RowGenerate(string name, object type, object val)
+        private static string RowGenerate(string name, object val)
         {
-            return name + _separator + type + _separator + val + _endRow;
+            return name + _separator + val + _endRow;
         }
     }
 }
