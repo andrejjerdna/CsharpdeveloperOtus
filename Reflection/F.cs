5e03c84b2f2e5b83508f0d80e48aa1005e89cf27
Степанов Андрей <33proekta@mail.ru>
2021-08-19 23:22:05 +0300

    Added DeserializeFromCsvToObject<T>

--- Reflection/F.cs
+++ Reflection/F.cs
@@ -1,4 +1,17 @@
-﻿namespace Reflection
+﻿using System.Dynamic;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Reflection
 {
-        class F { int i1, i2, i3, i4, i5;} 
+        class F
+        {
+            [JsonProperty]
+                int i1, i2, i3, i4, i5;
+
+                public static F Get()
+                {
+                    return new F() { i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 };
+                }
+        }
 }
